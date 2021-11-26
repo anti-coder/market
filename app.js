047de6332d2bc0ec6fb24d3d79c5228db683bcd3
jul <julandre@protonmail.com>
2021-11-26 17:11:51 +0300

    tuning VPS

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let express = require('express'); //подключаем модуль express
 let app = express(); //создали новый экземпляр класса express
 const data = 'node express work on 3000';
+let utc = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+
 
 app.listen(3000, function () {   //слушаем порт 3000
-  console.log('Обновлено 16-45');
+  console.log('Обновлено 17-10');
+  console.log(data);
 });
 
 app.use(express.json()); // установили метод express.json
 
 
 app.get('/', function (request, response) { 
-
-  response.send(data)
+  console.log(utc);
+  console.log(data);
+  response.send(utc)
 });
