112c4986ea038c2db003c874b98ffc26536e0154
jul <julandre@protonmail.com>
2021-11-26 04:14:32 +0300

    port 8089

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let express = require('express'); //подключаем модуль express
 let app = express(); //создали новый экземпляр класса express
 const data = '12345';
 
-app.listen(3000, function () {   //слушаем порт 3000
-  console.log('node express work on 3000');
+app.listen(8089, function () {   //слушаем порт 3000
+  console.log('node express work on 8089');
 });
 
 app.use(express.json()); // установили метод express.json
