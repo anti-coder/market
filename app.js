ec2e399700ec3aedac5fab7cc88c8d6e27d3d0f0
jul <julandre@protonmail.com>
2021-11-26 14:26:55 +0300

    tuning vps

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let express = require('express'); //подключаем модуль express
 let app = express(); //создали новый экземпляр класса express
-const data = '12345';
+const data = 'node express work on 3000';
 
 app.listen(8089, function () {   //слушаем порт 3000
-  console.log('node express work on 8089');
+  console.log('node express work on 3000');
 });
 
 app.use(express.json()); // установили метод express.json
