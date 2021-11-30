20236a5620d22ba05b94f9a0a6aff119ac2bfc8c
jul <julandre@protonmail.com>
2021-12-01 00:11:23 +0300

    tunung mysql

---
+++ app_tuning_VPS.js
@@ -0,0 +1,18 @@
+let express = require('express'); //подключаем модуль express
+let app = express(); //создали новый экземпляр класса express
+const data = 'слушаем порт 3000 в командной строке';
+const brow = '******************ответ браузера *************************1212121212'
+let utc = new Date();
+
+
+app.listen(3000, function () {   //слушаем порт 3000
+  console.log(utc);
+  console.log(data);
+});
+
+app.use(express.json()); // установили метод express.json
+
+
+app.get('/', function (request, response) { 
+  response.send(brow)
+});
